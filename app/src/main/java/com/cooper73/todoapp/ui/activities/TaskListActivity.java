f53ade1adc5f10a86b874e1417b9186aca23681c
Cristhian Cruz <c.cruzcoro73@gmail.com>
2023-03-01 18:03:33 -0500

    Added Task List activity

---
+++ app/src/main/java/com/cooper73/todoapp/ui/activities/TaskListActivity.java
@@ -0,0 +1,18 @@
+package com.cooper73.todoapp.ui.activities;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.cooper73.todoapp.R;
+
+public class TaskListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_task_list);
+
+
+    }
+}
