cb1412c07b59c6275b513da327fed60be657dbd3
Cristhian Cruz <c.cruzcoro73@gmail.com>
2023-03-06 17:25:57 -0500

    Added show task lists result and notify new task list methods

--- app/src/main/java/com/cooper73/todoapp/ui/views/HomeView.java
+++ app/src/main/java/com/cooper73/todoapp/ui/views/HomeView.java
@@ -2,11 +2,15 @@ package com.cooper73.todoapp.ui.views;
 
 import com.cooper73.todoapp.ui.viewmodels.TaskListViewModel;
 
+import java.util.ArrayList;
+
 public interface HomeView extends BaseView {
     void hideActionBar();
     void showProfile();
     void showImportantTasks();
     void showAllTasks();
+    void showTaskLists(ArrayList<TaskListViewModel> taskLists);
     void showTaskList(TaskListViewModel viewModel);
     void showCreateListDialog();
+    void notifyNewTaskListInserted(int position);
 }
