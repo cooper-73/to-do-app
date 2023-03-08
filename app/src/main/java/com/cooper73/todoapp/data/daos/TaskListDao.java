e60f0571d5241fa82994d01550e36dcb2704aac5
Cristhian Cruz <c.cruzcoro73@gmail.com>
2023-03-08 17:28:29 -0500

    Added instructions for getting task list by id, update task lists, and deleted them

--- app/src/main/java/com/cooper73/todoapp/data/daos/TaskListDao.java
+++ app/src/main/java/com/cooper73/todoapp/data/daos/TaskListDao.java
@@ -1,8 +1,10 @@
 package com.cooper73.todoapp.data.daos;
 
 import androidx.room.Dao;
+import androidx.room.Delete;
 import androidx.room.Insert;
 import androidx.room.Query;
+import androidx.room.Update;
 
 import com.cooper73.todoapp.data.entities.TaskList;
 
@@ -15,4 +17,13 @@ public interface TaskListDao {
 
     @Insert
     void insertNewTaskList(TaskList taskList);
+
+    @Query("SELECT * FROM task_lists WHERE id = :taskListId")
+    TaskList getById(String taskListId);
+
+    @Update
+    void updateTaskList(TaskList taskList);
+
+    @Delete
+    void deleteTaskList(TaskList taskList);
 }
