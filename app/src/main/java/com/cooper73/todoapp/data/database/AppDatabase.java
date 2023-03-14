85bcd11962c4cfa9d6bdd8c4fbc632cee82c8fad
Cristhian Cruz <c.cruzcoro73@gmail.com>
2023-03-14 17:59:40 -0500

    Added task dao

--- app/src/main/java/com/cooper73/todoapp/data/database/AppDatabase.java
+++ app/src/main/java/com/cooper73/todoapp/data/database/AppDatabase.java
@@ -5,15 +5,17 @@ import androidx.room.RoomDatabase;
 import androidx.room.TypeConverters;
 
 import com.cooper73.todoapp.data.converters.DateConverter;
+import com.cooper73.todoapp.data.daos.TaskDao;
 import com.cooper73.todoapp.data.daos.TaskListDao;
 import com.cooper73.todoapp.data.daos.UserDao;
 import com.cooper73.todoapp.data.entities.Task;
 import com.cooper73.todoapp.data.entities.TaskList;
 import com.cooper73.todoapp.data.entities.User;
 
-@Database(entities = {User.class, TaskList.class, Task.class}, version = 3)
+@Database(entities = {User.class, TaskList.class, Task.class}, version = 4)
 @TypeConverters({DateConverter.class})
 public abstract class AppDatabase extends RoomDatabase {
     public abstract UserDao userDao();
     public abstract TaskListDao taskListDao();
+    public abstract TaskDao taskDao();
 }
