5e884a720ef68dbe10fbc814119f83346641ef48
Cristhian Cruz <c.cruzcoro73@gmail.com>
2023-03-17 19:32:11 -0500

    Completed All Tasks interactor

--- app/src/main/java/com/cooper73/todoapp/data/interactors/AllTasksInteractorImpl.java
+++ app/src/main/java/com/cooper73/todoapp/data/interactors/AllTasksInteractorImpl.java
@@ -1,19 +1,58 @@
 package com.cooper73.todoapp.data.interactors;
 
+import com.cooper73.todoapp.MyApplication;
+import com.cooper73.todoapp.data.daos.TaskDao;
+import com.cooper73.todoapp.data.entities.Task;
+
+import java.util.List;
+
 public class AllTasksInteractorImpl implements AllTasksInteractor {
+    private final AllTasksInteractor.Callbacks callbacks;
+
+    public AllTasksInteractorImpl(Callbacks callbacks) {
+        this.callbacks = callbacks;
+    }
 
     @Override
     public void getAllTasks(String userId) {
-
+        TaskDao taskDao = MyApplication.getDatabase().taskDao();
+        MyApplication.getExecutorService().execute(() -> {
+            try {
+                List<Task> allTasks = taskDao.getAllTasksByUserId(userId);
+                callbacks.successGetAllTasks(allTasks);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
     }
 
     @Override
     public void updateTaskCompletedFlag(String taskId, boolean isCompleted, int position) {
-
+        TaskDao taskDao = MyApplication.getDatabase().taskDao();
+        MyApplication.getExecutorService().execute(() -> {
+            try {
+                Task task = taskDao.getById(taskId);
+                task.setCompleted(isCompleted);
+                taskDao.updateTask(task);
+                callbacks.successUpdateTaskCompletedFlag(position);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
     }
 
     @Override
     public void updateTaskImportantFlag(String taskId, boolean isImportant, int position) {
-
+        TaskDao taskDao = MyApplication.getDatabase().taskDao();
+        MyApplication.getExecutorService().execute(() -> {
+            try {
+                Task task = taskDao.getById(taskId);
+                task.setImportant(isImportant);
+                taskDao.updateTask(task);
+                callbacks.successUpdateTaskImportantFlag(position);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
     }
 }
