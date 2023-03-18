6ee4cda0a7bf12fc9f8aa089f84e75769dde7ede
Cristhian Cruz <c.cruzcoro73@gmail.com>
2023-03-17 22:32:48 -0500

    Completed All Tasks presenter and attached to activity with recycler view adapter created

--- app/src/main/java/com/cooper73/todoapp/presentation/presenters/AllTasksPresenterImpl.java
+++ app/src/main/java/com/cooper73/todoapp/presentation/presenters/AllTasksPresenterImpl.java
@@ -1,18 +1,17 @@
 package com.cooper73.todoapp.presentation.presenters;
 
+import com.cooper73.todoapp.MyApplication;
 import com.cooper73.todoapp.data.entities.Task;
 import com.cooper73.todoapp.data.interactors.AllTasksInteractor;
 import com.cooper73.todoapp.data.interactors.AllTasksInteractorImpl;
-import com.cooper73.todoapp.ui.viewmodels.TaskViewModel;
+import com.cooper73.todoapp.ui.mappers.TaskMapper;
 import com.cooper73.todoapp.ui.views.HighlightedTasksView;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class AllTasksPresenterImpl implements AllTasksPresenter, AllTasksInteractor.Callbacks {
     private final HighlightedTasksView view;
     private final AllTasksInteractor interactor;
-    private ArrayList<TaskViewModel> tasks;
 
     public AllTasksPresenterImpl(HighlightedTasksView view) {
         this.view = view;
@@ -31,21 +30,21 @@ public class AllTasksPresenterImpl implements AllTasksPresenter, AllTasksInterac
 
     @Override
     public void updateTaskImportantFlag(String taskId, boolean isImportant, int position) {
-        interactor.updateTaskCompletedFlag(taskId, isImportant, position);
+        interactor.updateTaskImportantFlag(taskId, isImportant, position);
     }
 
     @Override
     public void successGetAllTasks(List<Task> allTasks) {
-
+        MyApplication.getMainThreadHandler().post(() -> view.showTasks(TaskMapper.from(allTasks)));
     }
 
     @Override
     public void successUpdateTaskCompletedFlag(int position) {
-
+        MyApplication.getMainThreadHandler().post(() -> view.notifyTaskUpdated(position));
     }
 
     @Override
     public void successUpdateTaskImportantFlag(int position) {
-
+        MyApplication.getMainThreadHandler().post(() -> view.notifyTaskUpdated(position));
     }
 }
--- app/src/main/java/com/cooper73/todoapp/ui/activities/AllTasksActivity.java
+++ app/src/main/java/com/cooper73/todoapp/ui/activities/AllTasksActivity.java
@@ -11,13 +11,18 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.cooper73.todoapp.R;
+import com.cooper73.todoapp.presentation.presenters.AllTasksPresenter;
+import com.cooper73.todoapp.presentation.presenters.AllTasksPresenterImpl;
+import com.cooper73.todoapp.ui.adapters.TaskItemAdapter;
 import com.cooper73.todoapp.ui.viewmodels.TaskViewModel;
 import com.cooper73.todoapp.ui.views.HighlightedTasksView;
 
 import java.util.ArrayList;
 
-public class AllTasksActivity extends AppCompatActivity implements HighlightedTasksView {
+public class AllTasksActivity extends AppCompatActivity implements HighlightedTasksView, TaskItemAdapter.Listener {
     private RecyclerView tasksRecyclerView;
+    private AllTasksPresenter presenter;
+    private TaskItemAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,7 +37,7 @@ public class AllTasksActivity extends AppCompatActivity implements HighlightedTa
 
     @Override
     public void initPresenter() {
-
+        presenter = new AllTasksPresenterImpl(this);
     }
 
     @Override
@@ -69,16 +74,39 @@ public class AllTasksActivity extends AppCompatActivity implements HighlightedTa
 
     @Override
     public void showTasks(ArrayList<TaskViewModel> tasks) {
-
+        adapter = new TaskItemAdapter(tasks, this);
+        tasksRecyclerView.setAdapter(adapter);
     }
 
     @Override
     public void notifyTaskRemovedAndRangeChanged(int position, int itemCount) {
-
+        adapter.notifyItemRemoved(position);
+        adapter.notifyItemRangeChanged(position, itemCount);
     }
 
     @Override
     public void notifyTaskUpdated(int position) {
+        adapter.notifyItemChanged(position);
+    }
 
+    @Override
+    public void onCompletedCheckBoxClick(TaskViewModel task, boolean isCompleted, int position) {
+        presenter.updateTaskCompletedFlag(task.getId(), isCompleted, position);
+    }
+
+    @Override
+    public void onRecyclerItemClick(TaskViewModel task) {
+
+    }
+
+    @Override
+    public void onImportantCheckBoxClick(TaskViewModel task, boolean isImportant, int position) {
+        presenter.updateTaskImportantFlag(task.getId(), isImportant, position);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        presenter.loadAllTasks("d1e0c4fc-207c-41fb-ac57-98a383ec612e");
     }
 }
