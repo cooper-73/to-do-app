4f86a1bf9ce6344f98f92ce1a9f5b192bba143f7
Cristhian Cruz <c.cruzcoro73@gmail.com>
2023-03-18 16:31:03 -0500

    Completed Login activity UI

--- app/src/main/java/com/cooper73/todoapp/ui/activities/LoginActivity.java
+++ app/src/main/java/com/cooper73/todoapp/ui/activities/LoginActivity.java
@@ -1,16 +1,62 @@
 package com.cooper73.todoapp.ui.activities;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.cooper73.todoapp.R;
+import com.cooper73.todoapp.ui.views.LoginView;
 
-public class LoginActivity extends AppCompatActivity {
-
+public class LoginActivity extends AppCompatActivity implements LoginView {
+    private EditText emailEditText;
+    private Button loginButton;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        setActionBar();
+        initPresenter();
+        bindUI();
+        initUI();
+        initEvents();
+    }
+
+    @Override
+    public void initPresenter() {
+
+    }
+
+    @Override
+    public void bindUI() {
+        emailEditText = findViewById(R.id.et_login_email);
+        loginButton = findViewById(R.id.btn_login_login);
+    }
+
+    @Override
+    public void initUI() {
+
+    }
+
+    @Override
+    public void initEvents() {
+        loginButton.setOnClickListener(v -> onLoginButtonClick());
+    }
+
+    @Override
+    public void setActionBar() {
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setTitle("");
+        }
+    }
+
+    @Override
+    public void onLoginButtonClick() {
+        Toast.makeText(this, "Login...", Toast.LENGTH_SHORT).show();
     }
 }
---
+++ app/src/main/java/com/cooper73/todoapp/ui/views/LoginView.java
@@ -0,0 +1,6 @@
+package com.cooper73.todoapp.ui.views;
+
+public interface LoginView extends BaseView {
+    void setActionBar();
+    void onLoginButtonClick();
+}
