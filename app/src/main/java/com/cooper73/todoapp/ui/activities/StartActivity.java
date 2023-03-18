0fbfd7698f482a552ca940a08aeb468d929458a8
Cristhian Cruz <c.cruzcoro73@gmail.com>
2023-03-18 15:20:51 -0500

    Linked login and register activities

--- app/src/main/java/com/cooper73/todoapp/ui/activities/StartActivity.java
+++ app/src/main/java/com/cooper73/todoapp/ui/activities/StartActivity.java
@@ -1,13 +1,17 @@
 package com.cooper73.todoapp.ui.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.cooper73.todoapp.R;
 import com.cooper73.todoapp.ui.views.StartView;
 
 public class StartActivity extends AppCompatActivity implements StartView {
+    private Button loginButton, registerButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,7 +31,8 @@ public class StartActivity extends AppCompatActivity implements StartView {
 
     @Override
     public void bindUI() {
-
+        loginButton = findViewById(R.id.btn_start_login);
+        registerButton = findViewById(R.id.btn_start_register);
     }
 
     @Override
@@ -37,21 +42,27 @@ public class StartActivity extends AppCompatActivity implements StartView {
 
     @Override
     public void initEvents() {
-
+        loginButton.setOnClickListener(v -> goLoginActivity());
+        registerButton.setOnClickListener(v -> goRegisterActivity());
     }
 
     @Override
     public void hideActionBar() {
-
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.hide();
+        }
     }
 
     @Override
     public void goLoginActivity() {
-
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
     }
 
     @Override
     public void goRegisterActivity() {
-
+        Intent intent = new Intent(this, RegisterActivity.class);
+        startActivity(intent);
     }
 }
