4b433fdad095c5c79c402d2c71d594af919b4886
Cristhian Cruz <c.cruzcoro73@gmail.com>
2023-03-18 17:14:55 -0500

    Completed Register activity UI

--- app/src/main/java/com/cooper73/todoapp/ui/activities/RegisterActivity.java
+++ app/src/main/java/com/cooper73/todoapp/ui/activities/RegisterActivity.java
@@ -1,16 +1,64 @@
 package com.cooper73.todoapp.ui.activities;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.cooper73.todoapp.R;
+import com.cooper73.todoapp.ui.views.RegisterView;
 
-public class RegisterActivity extends AppCompatActivity {
-
+public class RegisterActivity extends AppCompatActivity implements RegisterView {
+    private EditText firstNameEditText, lastNameEditText, emailEditText;
+    private Button registerButton;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        setActionBar();
+        initPresenter();
+        bindUI();
+        initUI();
+        initEvents();
+    }
+
+    @Override
+    public void initPresenter() {
+        
+    }
+
+    @Override
+    public void bindUI() {
+        firstNameEditText = findViewById(R.id.et_register_first_name);
+        lastNameEditText = findViewById(R.id.et_register_last_name);
+        emailEditText = findViewById(R.id.et_register_email);
+        registerButton = findViewById(R.id.btn_register_register);
+    }
+
+    @Override
+    public void initUI() {
+
+    }
+
+    @Override
+    public void initEvents() {
+        registerButton.setOnClickListener(v -> onRegisterButtonClick());
+    }
+
+    @Override
+    public void setActionBar() {
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setTitle("");
+        }
+    }
+
+    @Override
+    public void onRegisterButtonClick() {
+        Toast.makeText(this, "Register...", Toast.LENGTH_SHORT).show();
     }
 }
---
+++ app/src/main/java/com/cooper73/todoapp/ui/views/RegisterView.java
@@ -0,0 +1,6 @@
+package com.cooper73.todoapp.ui.views;
+
+public interface RegisterView extends BaseView {
+    void setActionBar();
+    void onRegisterButtonClick();
+}
