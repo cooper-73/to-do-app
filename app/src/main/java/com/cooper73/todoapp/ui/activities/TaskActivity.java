bb5b7c22c4fc4e380cb426031f8ea93b429b4b64
Cristhian Cruz <c.cruzcoro73@gmail.com>
2023-03-21 16:39:07 -0500

    Created Task activity

---
+++ app/src/main/java/com/cooper73/todoapp/ui/activities/TaskActivity.java
@@ -0,0 +1,16 @@
+package com.cooper73.todoapp.ui.activities;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.cooper73.todoapp.R;
+
+public class TaskActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_task);
+    }
+}
